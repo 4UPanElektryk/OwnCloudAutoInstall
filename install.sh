840b72db932ada7763409381038822fcc526a794
4UPanElektryk <80508394+4UPanElektryk@users.noreply.github.com>
2023-01-13 21:22:30 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt update && sudo apt upgrade
+sudo apt install php7.4 php7.4-intl php7.4-mysql php7.4-mbstring php7.4-imagick php7.4-igbinary php7.4-gmp php7.4-bcmath php7.4-curl php7.4-gd php7.4-zip php7.4-imap php7.4-ldap php7.4-bz2 php7.4-ssh2 php7.4-common php7.4-json php7.4-xml php7.4-dev php7.4-apcu php7.4-redis libsmbclient-dev php-pear php-phpseclib
+sudo apt install smbclient redis-server unzip openssl rsync imagemagick
+sudo apt install libapache2-mod-php7.4 apache2
